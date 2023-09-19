c00c4ad1d726734ed4fbe3f7c0dbf41b3e53fd6d
seandong <sindon@gmail.com>
2023-09-19 16:20:20 +0800

    chore: init repo

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+var started = time.Now()
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(started)
+
+	if duration.Seconds() > 20 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("ok"))
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	host, _ := os.Hostname()
+	io.WriteString(w, fmt.Sprintf("[v4] Hello, Kubernetes! host: %s", host))
+}
+
+func main() {
+	http.HandleFunc("/healthz", healthz)
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":3000", nil)
+}
